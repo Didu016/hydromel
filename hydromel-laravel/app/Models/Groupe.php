006ae0f0fc2fa3048ab2b46d97b71076cf1e918f
spat <philippe.spat@heig-vd.ch>
2017-06-07 10:37:04 +0200

    First version of backend environnement
    First version of backend environnement for Hydrocontest project.

---
+++ hydromel-laravel/app/Models/Groupe.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Groupe extends Model
+{
+    protected $primaryKey = "Nom";
+    protected $incrementing = false;
+}
---
+++ hydromel-laravel/database/migrations/2017_06_06_120100_create_membership_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMembershipTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('membership', function (Blueprint $table) {
+            $table->integer('user_id')->unsigned();
+            $table->integer('group_id')->unsigned();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('group_id')->references('id')->on('groups');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::dropIfExists('membership');
+    }
+
+}
