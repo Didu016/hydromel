b43ed3e5f32d6ae19f8af962043b1d7cebf0593a
Didu016 <david.kostic@heig-vd.ch>
2017-06-16 12:17:32 +0200

    Update menu.blade.php

--- hydromel-laravel/resources/views/menu.blade.php
+++ hydromel-laravel/resources/views/menu.blade.php
@@ -1,10 +1,26 @@
 <nav>
-    <a href="index.blade.php"><img id="logoAccueil" src="img/logoHydro.png"></a>
+    <a href="#accueil"><img id="logoAccueil" src="img/logoHydro.png"></a>
     <ul>
         <li><a href="#accueil">Accueil</a></li>
         <li><a href="#equipe">L'équipe</a></li>
         <li><a href="#actualite">Actualités</a></li>
         <li><a href="#sponsor">Sponsors</a></li>
-        <li><a href="#editionprecedente">Edition précédentes</a></li>
+        <li><a href="#edition">Editions précédentes</a></li>
     </ul>
+    <div id="contactMenu">
+      <ul>
+          <li>CONTACT</li>
+          <li>079 792 34 19</li>
+          <li><a href="mailto:david.kostic@heig-vd.ch">Mail</a></li>
+          <li>
+              <a href="https://www.facebook.com/teamheigvd.hydrocontest/"><img class="logoContact" src="img/facebook.png"></a>
+          </li>
+          <li>
+            <a href="https://www.instagram.com/hydrocontest_heig_vd/?hl=fr"><img class="logoContact" src="img/instagram.png"></a>
+        </li>
+
+      </ul>
+
+
+  </div>
 </nav>
